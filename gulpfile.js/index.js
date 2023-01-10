c90a8f15df2d2a926f786330e32bd8664d91ffed
sihle <gubug777@gmail.com>
2023-01-10 12:07:04 +0800

    add gulp-babel & browser-sync

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,8 +2,9 @@ const { src, dest, series, parallel, watch } = require('gulp')
 const postcss = require('gulp-postcss')
 const sourcemaps = require('gulp-sourcemaps')
 const concat = require('gulp-concat')
-const babel = require('gulp-babel');
+const babel = require('gulp-babel')
 const clean = require('gulp-clean')
+const browserSync = require('browser-sync').create()
 
 const html = () => {
   /**
@@ -11,6 +12,7 @@ const html = () => {
    */
   return src('./src/**/*.html')
     .pipe(dest('./public'))
+    .pipe(browserSync.stream())
 }
 
 const css = () => {
@@ -25,6 +27,7 @@ const css = () => {
     .pipe(postcss())
     .pipe(sourcemaps.write('./maps')) // 指定 sourcemaps 輸出路徑
     .pipe(dest('./public/styles'))
+    .pipe(browserSync.stream())
 }
 
 const js = () => {
@@ -34,12 +37,15 @@ const js = () => {
    */
   return src('./src/assets/scripts/**/*.js')
     .pipe(sourcemaps.init()) // 初始化 sourcemaps
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
+    .pipe(
+      babel({
+        presets: ['@babel/env']
+      })
+    )
     .pipe(concat('all.js'))
     .pipe(sourcemaps.write('./maps')) // 指定 sourcemaps 輸出路徑
     .pipe(dest('./public/scripts'))
+    .pipe(browserSync.stream())
 }
 
 const cleanFiles = () => {
@@ -48,18 +54,32 @@ const cleanFiles = () => {
    * 1. read:false 關閉自動讀取以提升效能
    * 2. allowEmpty:true 允許 public 不存在時的例外錯誤
    */
-  return src('./public', {read: false, allowEmpty: true})
-    .pipe(clean())
+  return src('./public', { read: false, allowEmpty: true }).pipe(clean())
 }
 
-const watchFiles = (done) => {
+const watchFiles = done => {
   /**
    * 自動監測檔案變化，並進行檔案的複製、編譯異動
    * watch syntax: watch('glob string', task func)
    * 1. 移除 globs string {html,css,js} 語法，直接在 ./src 下搜索此語法，可能會浪費效能，應該修改為針對每個目錄進行監測
    */
+  browserSync.init({
+    server: {
+      baseDir: './public' // <= 指向虛擬伺服器需存取的資料夾
+    },
+    // port: 6600,
+    reloadDelay: 1000,
+  })
+
   watch('./src/**/*.html', html)
-  watch(['./src/**/*.html', './src/assets/styles/**/*.css', './src/assets/scripts/**/*.js'], css)
+  watch(
+    [
+      './src/**/*.html',
+      './src/assets/styles/**/*.css',
+      './src/assets/scripts/**/*.js'
+    ],
+    css
+  )
   watch('./src/assets/scripts/**/*.js', js)
 
   done()
